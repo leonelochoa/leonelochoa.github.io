5f9b2454ba296be777ff0f2e8beef5f588dcd690
Leonel Ochoa <37006313+leonelochoa@users.noreply.github.com>
2018-04-13 01:41:23 -0700

    Implemented Full-page js, restructured HTML

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-   //$("#wrapper").fullpage();
+   $("#wrapper").fullpage();
+
+   var typed = new Typed('.typed', {
+   	strings: ["Loading...", "Researcher.  ^175 Programmer.  ^175 Innovator.  "],
+    	typeSpeed: 85,
+        smartBackspace: false,
+        showCursor: false
+    });
 
 });
