aca1ae10b39551483c2cf4656aed61c90956ad62
Leonel Ochoa <37006313+leonelochoa@users.noreply.github.com>
2018-04-24 17:35:48 -0700

    About Redesign

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,18 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-   var typed = new Typed('#titles', {
-   	strings: ["Loading...", "Researcher.  ^175 Programmer.  ^175 Innovator.  "],
-    	typeSpeed: 30,
-        smartBackspace: false,
-        showCursor: false
+    $("#fullpage").fullpage({
+
     });
 
-   $("#fullpage").fullpage();
+    var typed = new Typed("#titles", {
+   	strings: ["Researcher. ^150 Programmer. ^150 Innovator."],
+    	typeSpeed: 75,
+    	smartBackspace: false,
+        showCursor: false,
+        backSpeed: -100000,
+        startDelay: 750
+
+    });
 
 });
